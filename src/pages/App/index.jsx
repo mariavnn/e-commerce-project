994a6e8859a87d9367dec510e6e67778e3fc8835
mariavnn <marianieto290520@gmail.com>
2024-08-01 10:43:26 -0500

    Initial commit

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,13 @@
+import Home from '../Home'
+import './App.css'
+
+function App() {
+  return(
+    <div className=''>
+      <p>Hello World</p>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
